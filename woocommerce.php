33b972bb92de4cf4d4d959f7c7053d95212f9027
Vencislav Atanasov <user890104@users.noreply.github.com>
2025-10-16 19:27:23 +0300

    Replace status buttons if order is COD and Processing

--- woocommerce.php
+++ woocommerce.php
@@ -1,4 +1,6 @@
 <?php
+use Automattic\WooCommerce\Enums\OrderStatus;
+
 /**
  * Change number of products that are displayed per page (shop page)
  */
@@ -28,3 +30,30 @@ add_filter('woocommerce_default_catalog_orderby', 'initlab_shop_woocommerce_defa
 function initlab_shop_woocommerce_default_catalog_orderby($default_orderby) {
 	return 'rand';
 }
+
+add_filter('woocommerce_admin_order_preview_actions', 'initlab_shop_woocommerce_admin_order_preview_actions', 10, 2);
+
+function initlab_shop_woocommerce_admin_order_preview_actions($actions, $order) {
+	if ($order->has_status([OrderStatus::PROCESSING]) && $order->get_payment_method() === 'cod') {
+		unset($actions['status']['actions']['complete']);
+
+		$actions['status']['actions'] = [
+			'pay-cash' => [
+				'url' => wp_nonce_url(admin_url('admin-ajax.php?action=init_lab_shop_cash_register_payment&method=cash&order_id=' . $order->get_id()), 'init_lab_shop_cash_register_payment'),
+				'name' => __('Pay in CASH', 'initlab-shop-addons'),
+				'title' => __('Pay the order in cash', 'initlab-shop-addons'),
+				'action' => 'pay-cash',
+			],
+			'pay-card' => [
+				'url' => wp_nonce_url(admin_url('admin-ajax.php?action=init_lab_shop_cash_register_payment&method=card&order_id=' . $order->get_id()), 'init_lab_shop_cash_register_payment'),
+				'name' => __('Pay by CARD', 'initlab-shop-addons'),
+				'title' => __('Pay the order by card', 'initlab-shop-addons'),
+				'action' => 'pay-card',
+			],
+		];
+	}
+
+	return $actions;
+}
+
+// TODO admin action init_lab_shop_cash_register_payment
